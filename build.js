01a9b3385b6fc99d345e4d10c694e75953569720
Naris R <nariscatboy@gmail.com>
2020-10-22 13:02:30 +1100

    initial

---
+++ build.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const pug = require('pug');
+const fs = require('fs');
+
+const PurgeCSS = require('purgecss').default;
+
+const html = pug.renderFile('src/index.pug')
+fs.writeFile('build/index.html', html, () => {});
+
+
+new PurgeCSS().purge({content: [{raw: html, extension: 'html'}, 'src/scripts.js'], css: ['src/styles.css']}).then(
+    css => css.map(
+        file => fs.writeFile(
+            file.file.replace('src', 'build'),
+            file.css,
+            () => {}
+        )
+    )
+);
