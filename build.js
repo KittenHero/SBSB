6fa099cb7c8c71c094189298da952aa5f9951b29
Naris R <nariscatboy@gmail.com>
2020-11-12 17:57:01 +1100

    add makefile

--- build.js
+++ build.js
@@ -1,26 +1,36 @@
 const path = require('path');
-const pug = require('pug');
 const fs = require('fs');
+const pug = require('pug');
 
-const PurgeCSS = require('purgecss').default;
-
+const args = process.argv.slice(2);
 const html = pug.renderFile('src/index.pug')
-fs.writeFile('build/index.html', html, () => {});
 
+if (args.includes('-all')) {
+    args.push('-html', '-css');
+}
 
-new PurgeCSS().purge({
-    content: [
-        {raw: html, extension: 'html'},
-        'src/scripts.js',
-        'node_modules/bootstrap/dist/js/bootstrap.min.js'
-    ],
-    css: ['src/styles.css']
-}).then(
-    css => css.map(
-        file => fs.writeFile(
-            file.file.replace('src', 'build'),
-            file.css,
-            () => {}
+if (args.includes('-html')) {
+    fs.writeFile('build/index.html', html, () => {});
+}
+
+if (args.includes('-css')) {
+    const PurgeCSS = require('purgecss').default;
+    new PurgeCSS().purge({
+        content: [
+            {raw: html, extension: 'html'},
+            'src/scripts.js',
+            'src/assets/mail/contact_me.js',
+            'src/assets/mail/jqBootstrapValidation.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js'
+        ],
+        css: ['src/styles.css']
+    }).then(
+        css => css.map(
+            file => fs.writeFile(
+                file.file.replace('src', 'build'),
+                file.css,
+                () => {}
+            )
         )
-    )
-);
+    );
+}
---
+++ makefile
@@ -0,0 +1,51 @@
+CC=npm run build --
+
+BUILDPATH=build/
+SRCDIR=src/
+HTML=$(BUILDPATH)index.html
+HTMLSRC:=$(shell find $(SRCDIR) -name '*.pug')
+CSS=$(BUILDPATH)styles.css
+CSSSRC:=$(shell find $(SRCDIR) -regex '.*\\.\\(pug\\|css\\|js\\)')
+ASSETSSRC:=$(shell find $(SRCDIR) -regex '.*\\.\\(js\\|png\\|jpg\\|svg\\|ico\\)')
+ASSETS:=$(subst $(SRCDIR), $(BUILDPATH), $(ASSETSSRC))
+BUILDSUBDIRS:=$(shell echo $(dir $(ASSETS))|tr ' ' '\n'|sort -u|tr '\n' ' ')
+
+.PHONY: all clean
+
+all: $(HTML) $(CSS) $(ASSETS)
+
+clean:
+	rm -rf $(BUILDPATH)
+
+$(HTML): $(HTMLSRC) | $(BUILDPATH)
+	@echo $@
+	@$(CC) -html
+
+$(CSS): $(CSSSRC) | $(BUILDPATH)
+	@echo $@
+	@$(CC) -css
+
+$(BUILDSUBDIRS):
+	@mkdir -p $@
+
+.SECONDEXPANSION:
+
+$(BUILDPATH)%.jpg: $(SRCDIR)%.jpg | $(BUILDSUBDIRS)
+	@echo $@
+	@cp $< $@
+
+$(BUILDPATH)%.png: $(SRCDIR)%.png | $(BUILDSUBDIRS)
+	@echo $@
+	@cp $< $@
+
+$(BUILDPATH)%.svg: $(SRCDIR)%.svg | $(BUILDSUBDIRS)
+	@echo $@
+	@cp $< $@
+
+$(BUILDPATH)%.ico: $(SRCDIR)%.ico | $(BUILDSUBDIRS)
+	@echo $@
+	@cp $< $@
+
+$(BUILDPATH)%.js: $(SRCDIR)%.js | $(BUILDSUBDIRS)
+	@echo $@
+	@cp $< $@
